5462bee0f92c11a4ad9193fcc235e3ceddb0b3f6
David Johnson <dave@badgersoft.com>
2018-10-11 13:25:04 +0100

    RealTime Page basic content

--- fc-raw-data-war/src/main/java/com/badgersoft/datawarehouse/rawdata/service/impl/SatelliteListServiceImpl.java
+++ fc-raw-data-war/src/main/java/com/badgersoft/datawarehouse/rawdata/service/impl/SatelliteListServiceImpl.java
@@ -12,8 +12,8 @@ public class SatelliteListServiceImpl implements SatelliteListService {
     public List<Satellite> findAllSatellites() {
         List<Satellite> satellites = new ArrayList<>();
         satellites.add(new Satellite("ESEO", "eseo", "eseo_200.png", true));
-        satellites.add(new Satellite("FUNcube-1 (Flight)", "fc1-fm", "funcube-1_200.png", true));
-        satellites.add(new Satellite("JY1Sat (Flight)", "jy1sat-fm", "jy1sat_200.png", true));
+        satellites.add(new Satellite("FUNcube-1", "fc1-fm", "funcube-1_200.png", true));
+        satellites.add(new Satellite("JY1Sat", "jy1sat-fm", "jy1sat_200.png", true));
         satellites.add(new Satellite("Nayif-1", "nayif1", "nayif-1_200.png", true));
         satellites.add(new Satellite("UKube-1", "ukube1", "ukube-1_200.png", true));
         return satellites;
