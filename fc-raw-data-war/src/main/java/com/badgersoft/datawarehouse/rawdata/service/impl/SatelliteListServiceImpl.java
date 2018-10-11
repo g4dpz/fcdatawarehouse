9768d0a6b194f2712661b5a8531b861ebde21330
David Johnson <dave@badgersoft.com>
2018-10-11 09:02:08 +0100

    Removed engineerig and non flight ready satellites from list

--- fc-raw-data-war/src/main/java/com/badgersoft/datawarehouse/rawdata/service/impl/SatelliteListServiceImpl.java
+++ fc-raw-data-war/src/main/java/com/badgersoft/datawarehouse/rawdata/service/impl/SatelliteListServiceImpl.java
@@ -11,14 +11,11 @@ public class SatelliteListServiceImpl implements SatelliteListService {
     @Override
     public List<Satellite> findAllSatellites() {
         List<Satellite> satellites = new ArrayList<>();
-        satellites.add(new Satellite("FUNcube-1 (Flight)", "/fc1-fm", "funcube-1_200.png", true));
-        satellites.add(new Satellite("FUNcube-1 (Engineering)", "/fc1-em", "funcube-1_200.png", false));
-        satellites.add(new Satellite("UKube-1", "/ukube1", "ukube-1_200.png", true));
-        satellites.add(new Satellite("Nayif-1", "/nayif1", "nayif-1_200.png", true));
-        satellites.add(new Satellite("JY1Sat (Flight)", "/jy1sat-fm", "jy1sat_200.png", true));
-        satellites.add(new Satellite("JY1Sat (Engineering)", "/jy1sat-em", "jy1sat_200.png", false));
         satellites.add(new Satellite("ESEO", "/eseo", "eseo_200.png", true));
-        satellites.add(new Satellite("ESA Deep Space Gateway", "/esa-dsg", "deepspace_200.png", false));
+        satellites.add(new Satellite("FUNcube-1 (Flight)", "/fc1-fm", "funcube-1_200.png", true));
+        satellites.add(new Satellite("JY1Sat (Flight)", "/jy1sat-fm", "jy1sat_200.png", true));
+        satellites.add(new Satellite("Nayif-1", "/nayif1", "nayif-1_200.png", true));
+        satellites.add(new Satellite("UKube-1", "/ukube1", "ukube-1_200.png", true));
         return satellites;
     }
 }
