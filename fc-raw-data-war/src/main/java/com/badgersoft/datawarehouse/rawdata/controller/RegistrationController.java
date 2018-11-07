ccbd6e441ea5593721023f1e2b6667228099536a
David Johnson <dave@badgersoft.com>
2018-11-07 13:56:46 +0000

    Registration WIP

--- fc-raw-data-war/src/main/java/com/badgersoft/datawarehouse/rawdata/controller/RegistrationController.java
+++ fc-raw-data-war/src/main/java/com/badgersoft/datawarehouse/rawdata/controller/RegistrationController.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -39,6 +40,12 @@ public class RegistrationController {
     @GetMapping("registration")
     public ModelAndView registration() {
         ModelAndView modelAndView = new ModelAndView("content/registration");
+        return modelAndView;
+    }
+
+    @PostMapping("registration")
+    public ModelAndView doRegistration() {
+        ModelAndView modelAndView = new ModelAndView("content/registration-confirmation");
         return modelAndView;
     }
 }
